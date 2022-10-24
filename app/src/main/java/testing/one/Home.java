c4505888b99fb061ba8067fb7f342ff02de274fa
RonaldTayie <1999roniengwe@gmail.com>
2022-10-24 20:32:46 +0200

    Added RecyclerView for Issues

--- app/src/main/java/testing/one/Home.java
+++ app/src/main/java/testing/one/Home.java
@@ -1,6 +1,7 @@
 package testing.one;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
--- app/src/main/java/testing/one/HomeActivity.java
+++ app/src/main/java/testing/one/HomeActivity.java
@@ -1,14 +1,30 @@
 package testing.one;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import RecyclerViewAdaptor.IssuesRecyclerView;
+import data.Data;
+
 public class HomeActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    private Data data = new Data();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        recyclerView = findViewById(R.id.IssuesRecyclerView);
+
+        IssuesRecyclerView issuesRecyclerView = new IssuesRecyclerView(this,data.issues);
+
+        recyclerView.setAdapter(issuesRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
