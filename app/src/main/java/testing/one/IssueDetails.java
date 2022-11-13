d5c4354e5392c9ad612fcab19975ea519b902626
MbongX <mbongenimahlangu203@outlook.com>
2022-11-13 19:39:39 +0200

    Overall application is functional, works as intended

--- app/src/main/java/testing/one/IssueDetails.java
+++ app/src/main/java/testing/one/IssueDetails.java
@@ -23,47 +23,8 @@ String prac_sex = "@strings/Practice_Safe_Sex_Paragraph";
         title.setText(intentTitle);
 
 
-        //checking the title to see on what to populate in the scrollView
 
-        switch (intentTitle)
-        {
-            //under take Care
-            case "Practice Safe Sex":
-                Intent x = new Intent(this,SafeSex.class);
-                startActivity(x);
-                break;
-            case "Menstruation and Reproduction":
-                //throw in the data into the TextView
-                break;
-            case "Lack of interest in sex":
-                //throw in the data into the TextView
-                break;
-            case "Painful Sex":
-                //throw in the data into the TextView
-                break;
-            case "Problems with orgasm":
-                //throw in the data into the TextView
-                break;
-                //preventing Section
-            case "Preventing HIV with PrEP":
-                //throw in the data into the TextView
-                break;
-            case "Preventive vaccines":
-                //throw in the data into the TextView
-                break;
-            case "Get screened for STIs":
-                //throw in the data into the TextView
-                break;
-            case "Get regular Pap smears":
-                //throw in the data into the TextView
-                break;
-                //Emergency Contraceptives Section
-            case "Emergency contraception":
-                //throw in the data into the TextView
-                break;
-            default:
-                paragragh.setText("Error");
-        }
+
 
 
 
